c3c649df133e593d29e84c471ee8fa62a4832a2c
prashanth2031 <prashanthprashu.2031>
2025-12-20 19:36:34 +0530

    prediction

--- Prediction.py
+++
@@ -1,5 +0,0 @@
-import math
-import yfinance as yf
-import numpy as np
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
---
+++ STOCKPREDICTION..py
@@ -0,0 +1,9 @@
+import math
+import yfinance as yf
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import matplotlib.pyplot as plt
+plt.style.use ('fivethirtyeight')
